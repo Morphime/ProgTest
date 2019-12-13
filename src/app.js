ff3457c603bd0d289d656601b62ebe39b9eb2aeb
Francis <francissimard002@hotmail.com>
2019-12-12 22:15:50 -0500

    Ajout de mon premier single file component et de l'app qui fonctionne avec celui-ci

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 import Vue from 'vue';
+import Requestform from "./components/Requestform";
+
 
 new Vue({
     el: '#app',
-    template: '<App/>',
-    data: "test"
-})
+
+    components:{
+      Requestform,
+    }
+});
+
+// new Vue({
+//     el: '#app',
+//
+//     components:{
+//       Accordion,
+//     },
+//
+//     data: function () {
+//         return {
+//             items: [
+//                 {id: 1, title: 'Title 1', description: 'description 1'},
+//                 {id: 2, title: 'Title 2', description: 'description 2'},
+//                 {id: 3, title: 'Title 3', description: 'description 3'}
+//             ]
+//         }
+//
+//     }
+// });
---
+++ src/components/Requestform.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h3>Demande</h3>
+        <h4>Cases</h4>
+        <select selected>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Requestform"
+    }
+</script>
+
+<style scoped>
+
+</style>
